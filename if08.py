bfbb4dbbce3973097cc14a9172709a7925bb454a
allamurodxakimov <xakimov2806@gmail.com>
2023-09-27 13:47:37 -0700

    if *

--- if08.py
+++ if08.py
@@ -11,4 +11,12 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a%10>=1 and a%2==1:
+        return "two-digit odd number"
+    if a%10>=1 and a%2==0:
+        return "two-digit even number"
+    if a%100>=1 and a%2==1:
+        return "three-digit odd number"
+    if a%100>=1 and a%2==0:
+        return "three-digit even number"
+print(main(345))
