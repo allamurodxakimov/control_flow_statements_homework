bc6d7f1e872870273b57ebfadc3594b4827a5ebe
allamurodxakimov <xakimov2806@gmail.com>
2023-09-27 13:43:59 -0700

    if 3

--- if03.py
+++ if03.py
@@ -7,4 +7,10 @@ def main(a):
     Returns:
         a: integer
     """
-    return
+    if a>0:
+        return a+1
+    if a<0:
+        return a-2
+    if a==0:
+        return a + 10
+print(main(0))
